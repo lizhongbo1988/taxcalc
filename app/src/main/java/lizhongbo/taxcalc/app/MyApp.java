78ced67108e56483be3bd000f0e0233aed3df42e
flying <flying@kcmds.com>
2018-04-24 12:11:08 +0800

    调动主界面

--- app/src/main/java/lizhongbo/taxcalc/app/MyApp.java
+++ app/src/main/java/lizhongbo/taxcalc/app/MyApp.java
@@ -1,6 +1,7 @@
 package lizhongbo.taxcalc.app;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
@@ -12,10 +13,15 @@ import lizhongbo.taxcalc.beans.TaxRate;
 
 public class MyApp extends Application {
 
+    public static final String FILE_NAME = "taxrate";
+    public static final String KEY_START_TAX = "start_tax";
      @Override
     public void onCreate(){
          super.onCreate();
          FlowManager.init(this);
+         //读取起征点
+         SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+         TaxRateManager.startTaxIncome = sharedPreferences.getInt(KEY_START_TAX, 3500);
          /*第一次启动APP时，如果税率表为空，则添加默认税率到数据库*/
          List<TaxRate> taxRates = SQLite.select().from(TaxRate.class).queryList();
          if(taxRates == null || taxRates .size() == 0){
--- app/src/main/java/lizhongbo/taxcalc/ui/TaxRateActivity.java
+++ app/src/main/java/lizhongbo/taxcalc/ui/TaxRateActivity.java
@@ -1,33 +1,75 @@
 package lizhongbo.taxcalc.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.raizlabs.android.dbflow.sql.language.Operator;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import java.util.List;
 
 import lizhongbo.taxcalc.R;
+import lizhongbo.taxcalc.TaxRateManager;
+import lizhongbo.taxcalc.app.MyApp;
 import lizhongbo.taxcalc.beans.TaxRate;
 
-public class TaxRateActivity extends AppCompatActivity {
+public class TaxRateActivity extends AppCompatActivity implements View.OnClickListener {
     private ListView mListView;
     private TaxRateAdapter mTaxRateAdapter;
+    private EditText mStartTaxEdit;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taxrate);
 
-        mListView = findViewById(R.id.taxrate_list);
-        mTaxRateAdapter = new TaxRateAdapter(this);
+
     }
+
     @Override
     protected void onResume() {
         super.onResume();
+        mListView = findViewById(R.id.taxrate_list);
+        mTaxRateAdapter = new TaxRateAdapter(this);
+        Button addButton = findViewById(R.id.addtaxButton);
+        addButton.setOnClickListener(this);
 
-        List<TaxRate> taxRateList= SQLite.select().from(TaxRate.class).queryList();
+        List<TaxRate> taxRateList = SQLite.select().from(TaxRate.class).queryList();
         mTaxRateAdapter.setTaxRateList(taxRateList);
         mListView.setAdapter(mTaxRateAdapter);
+
+        mStartTaxEdit = findViewById(R.id.startTaxEditText);
+        mStartTaxEdit.setText(Integer.toString(TaxRateManager.startTaxIncome));
+        Button saveStartTaxButton = findViewById(R.id.saveStartTaxButton);
+        saveStartTaxButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addtaxButton:
+                break;
+            case R.id.saveStartTaxButton:
+                String s = mStartTaxEdit.getText().toString();
+                if(!TextUtils.isEmpty(s)){
+                    SharedPreferences sharedPreferences = getSharedPreferences(MyApp.FILE_NAME, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putInt(MyApp.KEY_START_TAX, Integer.getInteger(s));
+                    editor.commit();
+                }
+
+                break;
+            default:
+                break;
+        }
     }
 }
